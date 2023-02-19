4bb80f73eaf26c6f7e73af5d6116004ae3a3aa8c
Wojtek08 <114952767+Wojtek08@users.noreply.github.com>
2023-02-19 14:39:46 +0100

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,51 @@
+var button = document.getElementById("menu-button");
+var menuContent = document.getElementById("menu-content");
+var burger = document.getElementById("img-burger");
+
+var quizContent = document.getElementById("quiz-content");
+var chevronAsideDiv = document.getElementById("chevron-aside-div");
+var chevronAside = document.getElementById("chevron-aside");
+
+var quizButton = document.getElementById("quiz-button");
+var quizImg = document.getElementById("img-quiz");
+var quizContent = document.getElementById("quiz-content")
+
+
+quizButton.onclick = function () {
+    if (quizContent.className == "") {
+        quizContent.className = "open";
+        quizImg.src = "../../../assets/CloseWhite.svg";
+    } else if (quizContent.className == "open") {
+        quizContent.className = "";
+        quizImg.src = "../../../assets/Q.svg";
+    };
+};
+
+
+button.onclick = function () {
+    if (menuContent.className == "open") {
+        menuContent.className = "";
+    } else if (menuContent.className == "") {
+        menuContent.className = "open";
+    };
+
+
+    if (menuContent.className == "open") {
+        burger.src = "../../../assets/CloseWhite.svg";
+        burger.style.transform = "translateY(-2px)";
+    } else if (menuContent.className == "") {
+        burger.src = "../../../assets/BurgerWhite.svg";
+        burger.style.transform = "translateY(2px)";
+    };
+};
+
+
+chevronAsideDiv.onclick = function () {
+    if (quizContent.className == "") {
+        quizContent.className = "open";
+        chevronAside.style.transform = "rotate(0deg) translateY(2px)";
+    } else if (quizContent.className == "open") {
+        quizContent.className = "";
+        chevronAside.style.transform = "rotate(180deg) translateY(2px)";
+    };
+};
